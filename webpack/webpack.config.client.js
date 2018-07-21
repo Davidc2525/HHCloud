ff5ec5dd7d6de23bba54a1c5833b27e98d594f71
Davidc2525 <david25pcxtreme@gmail.com>
2018-07-21 16:34:26 -0400

    Update webpack.config.client.js

--- webpack/webpack.config.client.js
+++ webpack/webpack.config.client.js
@@ -38,17 +38,17 @@ export default [{
 		client: "./src/client/SucreCloudIndex/index.js",
 		c:["react","react-dom","lodash"],
 	},
-	output: {
+	output:  {
 		globalObject: "this",
 		hotUpdateChunkFilename: "[id].[hash].hot-update.js",
 		//publicPath:"/dist",
-		publicPath: '',
+		publicPath: '/home/gabriel/david/electron-quick-start/dist/SC/',
 		filename: 'assets/js/[name].js',
 		jsonpFunction: "_o",
 		pathinfo: true,
 		chunkFilename: 'assets/js/[name]_[chunkhash].js',
 
-		path: path.resolve(__dirname, '../dist/SC')
+		path: path.resolve(__dirname, '/home/gabriel/david/electron-quick-start/dist/SC')
 	},
 	devServer: {
 		host: "localhost",
